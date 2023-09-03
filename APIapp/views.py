d7e3c509cfbed11b824b2b95d765ed5e47de0bc0
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-09-03 07:01:19 +0530

    added basic APIview

--- APIapp/views.py
+++ APIapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+an_apiview = [
+            'uses HTTP methods as function (get, post, put, patch, delete)',
+            'Is similar to a traditional django view',
+            'gives you the most control over your application logic',
+            'is mapped manually to URLs',
+        ]
+
+def home(request):
+    global an_apiview
+    context = {
+        'an_apiview':an_apiview
+    }
+    return render(request,'home.html',context)
+
+def login(request):
+    return render(request,'login.html')
+
+class HelloApiView(APIView):
+    '''Test API View'''
+
+    def get(self, request, format=None):
+        '''Returns a list of APIView Items'''
+        global an_apiview
+        return Response({'message':'Hello..!','an_apiview':an_apiview})
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 This is an OAuth REST API project.
+
+DJANGO REST Framework Views: 1. API View    2.ViewSet
+
+1. API View:
+    Is a basic to build our API , it enables us to describe the logic which makes our API endpoint.
+    API View allows us to define functions that match the standard HTTP methods :
+    HTTP GET: to get one or more items
+    HTTP POST: to create an item
+    HTTP PUT: to update a whole item
+    HTTP PATCH: to update a particular field of an item
+    HTTP DELETE: to delete an item
+
+    Gives us the most control over the logic:
+        - perfect for implementing complex logic
+        - Calling other API's
+        - Working with local files.
